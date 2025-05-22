9b5c77e8e121a94cfef7a69f1948eefbafac44dc
blidarul <andyblidaru@gmail.com>
2025-05-22 20:58:35 +0300

    tema 11 rucsac done

--- tema-11-extra/rucsac.c
+++ tema-11-extra/rucsac.c
@@ -8,8 +8,43 @@
 
 #include <stdio.h>
 
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
 int main()
 {
+    #define n 10 // numarul greutatilor
+    int w[n + 1] = {-1,  23,  26,  20,  18,  32,  27,  29,  26,  30,  27};
+    int v[n + 1] = {-1, 505, 352, 458, 220, 354, 414, 498, 545, 473, 543};
+    const int capacitate_rucsac = 67;
+    int m[n + 1][capacitate_rucsac + 1];
+
+    for(int i = 0; i <= capacitate_rucsac; i++)
+    {
+        m[0][i] = 0;
+    }
+    for(int i = 0; i <= n; i++)
+    {
+        m[i][0] = 0;
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= capacitate_rucsac; j++)
+        {
+            if(w[i] > j)
+            {
+                m[i][j] = m[i - 1][j];
+            }
+            else
+            {
+                m[i][j] = max(m[i - 1][j], m[i - 1][j - w[i]] + v[i]);
+            }
+        }
+    }
+    printf("%d\n", m[n][capacitate_rucsac]);
 
     return 0;
 }
